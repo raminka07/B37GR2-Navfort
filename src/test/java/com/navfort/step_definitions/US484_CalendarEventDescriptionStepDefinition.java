816aa75d72f7ab1a31b67f2ce29122bf7daa64ca
sarakarji <sarakarji86@gmail.com>
2024-12-24 09:27:56 -0600

    I made some minor changes for US 484, based on what Troy explained today in daily stand up meeting. Also I changed feature and stepDefinition files' name based on naming convention.

--- src/test/java/com/navfort/step_definitions/US484_CalendarEventDescriptionStepDefinition.java
+++ src/test/java/com/navfort/step_definitions/US484_CalendarEventDescriptionStepDefinition.java
@@ -3,33 +3,20 @@ package com.navfort.step_definitions;
 
 import com.navfort.pages.*;
 import com.navfort.utilities.BrowserUtils;
-import com.navfort.utilities.ConfigurationReader;
 import com.navfort.utilities.Driver;
 import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 import org.openqa.selenium.interactions.Actions;
 
-public class US484StepDefinition {
-
-    LoginPage loginPage = new LoginPage();
-
-
-    @Given("the user logs in with username {string} and password {string}")
-    public void theUserLogsInWithUsernameAndPassword(String username, String password) {
-        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
-
-        loginPage.login(username, password);
-    }
-
-
+public class US484_CalendarEventDescriptionStepDefinition {
 
     US484BasePage basePage = new US484BasePage();
 
     @And("the user hover over {string} and selects {string} from the dropdown")
     public void theUserHoverOverAndSelectsFromTheDropdown(String modulebtn, String calendarEvent) {
+        BrowserUtils.waitForVisibility(basePage.activeties,500);
         Actions actions = new Actions(Driver.getDriver());
         actions.moveToElement(basePage.activeties).perform();
         BrowserUtils.sleep(3);
@@ -49,8 +36,8 @@ public class US484StepDefinition {
     }
 
 
-
     US484CreateEventPage createEventPage = new US484CreateEventPage();
+
     @When("the user types {string} into the text area")
     public void the_user_types_into_the_field(String msg) {
         BrowserUtils.waitForVisibility(createEventPage.iframe,300);
@@ -68,5 +55,4 @@ public class US484StepDefinition {
 
 
     }
-
 }
