b0a679bc19577fe535a68034aae10756e4457e81
donnellreuben <dreuben@dinklabs.io>
2024-12-30 13:43:19 -0600

    US475 AC #1 finished as well as UserStory, DONE

--- src/test/java/com/navfort/pages/MainModulesPage.java
+++ src/test/java/com/navfort/pages/MainModulesPage.java
@@ -10,16 +10,37 @@ public class MainModulesPage {
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
-    @FindBy(xpath = "//*[@class='title title-level-1'][contains(text(), ' Fleet')]")
+
+    //Dashboards
+    @FindBy(xpath = "//*[@class='title title-level-1'][contains(text(), 'Dashboards')]")
+    public WebElement dashboardBtn;
+
+    //Fleet
+    @FindBy(xpath = "//*[@class='title title-level-1'][contains(text(), 'Fleet')]")
     public WebElement fleetBtn;
 
-    @FindBy(xpath = "//*[@class='title title-level-1'][contains(text(), ' Customers')]")
+    //Customers
+    @FindBy(xpath = "//*[@class='title title-level-1' and contains(text(),'Customers')]")
     public WebElement customersBtn;
 
-    @FindBy(xpath = "//*[@class='title title-level-1'][contains(text(), 'Activities')]")
+    //Sales
+    @FindBy(xpath = "//*[@class='title title-level-1' and contains(text(),'Sales')]")
+    public WebElement salesBtn;
+
+    //Activities
+    @FindBy(xpath = "//*[@class='title title-level-1' and contains(text(),'Activities')]")
     public WebElement activitiesBtn;
 
-    @FindBy(xpath = "//*[@class='title title-level-1'][contains(text(), 'System')]")
+    //Marketing
+    @FindBy(xpath = "//*[@class='title title-level-1' and contains(text(),'Marketing')]")
+    public WebElement marketingBtn;
+
+    //Reports & Segments
+    @FindBy(xpath = "//*[@class='title title-level-1' and contains(text(),'Reports')]")
+    public WebElement reportsBtn;
+
+    //System
+    @FindBy(xpath = "//*[@class='title title-level-1' and contains(text(),'System')]")
     public WebElement systemBtn;
 
 }
--- src/test/java/com/navfort/step_definitions/US475_MainModulesStepDefs.java
+++ src/test/java/com/navfort/step_definitions/US475_MainModulesStepDefs.java
@@ -1,16 +1,12 @@
 package com.navfort.step_definitions;
 import com.navfort.pages.MainModulesPage;
-import com.navfort.utilities.BrowserUtils;
 import com.navfort.utilities.Driver;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 
 
 public class US475_MainModulesStepDefs {
@@ -18,10 +14,8 @@ public class US475_MainModulesStepDefs {
     MainModulesPage mainModulesPage = new MainModulesPage();
 
 
-    @Then("user sees {int} modules: {string}, {string}, {string}, {string}")
-    public void user_sees_modules(Integer int1, String expectedModule1, String expectedModule2, String expectedModule3, String expectedModule4) {
-
-        WebDriver driver = Driver.getDriver();
+    @Then("user sees four modules: {string}, {string}, {string}, {string}")
+    public void user_sees_modules(String expectedModule1, String expectedModule2, String expectedModule3, String expectedModule4) {
 
         //BrowserUtils.waitForVisibility(mainModulesPage.fleetBtn, 10);
 
@@ -39,6 +33,39 @@ public class US475_MainModulesStepDefs {
         Assert.assertEquals(expectedModule4, actualModule4);
     }
 
+
+
+
+
+
+
+    @Then("user sees eight modules: {string}, {string}, {string}, {string}, {string}, {string}, {string}, {string}")
+    public void user_sees_modules(String expectedModule1, String expectedModule2, String expectedModule3,
+                                  String expectedModule4, String expectedModule5, String expectedModule6,
+                                  String expectedModule7, String expectedModule8) {
+
+        WebDriverWait wait = new WebDriverWait( Driver.getDriver(), Duration.ofSeconds(10) );
+        wait.until(ExpectedConditions.visibilityOf(mainModulesPage.dashboardBtn));
+
+        String actualModule1 = mainModulesPage.dashboardBtn.getText();
+        String actualModule2 = mainModulesPage.fleetBtn.getText();
+        String actualModule3 = mainModulesPage.customersBtn.getText();
+        String actualModule4 = mainModulesPage.salesBtn.getText();
+        String actualModule5 = mainModulesPage.activitiesBtn.getText();
+        String actualModule6 = mainModulesPage.marketingBtn.getText();
+        String actualModule7 = mainModulesPage.reportsBtn.getText();
+        String actualModule8 = mainModulesPage.systemBtn.getText();
+
+
+        Assert.assertEquals(expectedModule1, actualModule1);
+        Assert.assertEquals(expectedModule2, actualModule2);
+        Assert.assertEquals(expectedModule3, actualModule3);
+        Assert.assertEquals(expectedModule4, actualModule4);
+        Assert.assertEquals(expectedModule5, actualModule5);
+        Assert.assertEquals(expectedModule6, actualModule6);
+        Assert.assertEquals(expectedModule7, actualModule7);
+        Assert.assertEquals(expectedModule8, actualModule8);
+    }
 
 
 }
